c286005777ec54c24823784fd46ce28674e960b5
Ashok N T <ANT2@ANT2W10BLR.dsone.3ds.com>
2018-06-06 08:45:39 +0530

    Committing Project

---
+++ src/main/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/App.java
@@ -0,0 +1,13 @@
+package SpecialBatch3_POM_FW.SpecialBatch3_POM_FW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/AppTest.java
@@ -0,0 +1,38 @@
+package SpecialBatch3_POM_FW.SpecialBatch3_POM_FW;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
