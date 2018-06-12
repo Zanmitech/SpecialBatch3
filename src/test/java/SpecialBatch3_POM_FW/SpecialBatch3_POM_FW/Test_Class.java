41efc0380c44988143c3fb2b894a1f269e3e8e5b
Ashok N T <Ant2@ANT2W10BLR.dsone.3ds.com>
2018-06-12 14:09:37 +0530

    Adding TEst Ng Code

---
+++ src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_Class.java
@@ -0,0 +1,12 @@
+package SpecialBatch3_POM_FW.SpecialBatch3_POM_FW;
+
+import org.testng.annotations.Test;
+
+public class Test_Class 
+{
+	@Test
+	public void TestMethod1()
+	{
+		System.out.println("Hello first statement from Test_Class");
+	}
+}
