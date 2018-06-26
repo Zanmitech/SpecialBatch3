cdb3c9f0d66827a9669cc2f2110f628dbf45f053
Ashok N T <ANT2@ANT2W10BLR.dsone.3ds.com>
2018-06-26 15:13:46 +0530

    Chiranjeeb please check the class name before check-in

--- src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
+++ src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
@@ -2,7 +2,7 @@ package SpecialBatch3_POM_FW.SpecialBatch3_POM_FW;
 
 import org.testng.annotations.Test;
 
-public class Test_Class2 {
+public class Test_class2 {
 	
 	@Test
 	public void TestMethod1()
