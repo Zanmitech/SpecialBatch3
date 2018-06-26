fec31d4b87d4007aa8fd971be90e990650e5c455
ashokuser2 <39877557+ashokuser2@users.noreply.github.com>
2018-06-26 14:40:09 +0530

    Update Test_class2.java
    Changing from GitHub

--- src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
+++ src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
@@ -7,6 +7,7 @@ public class Test_Class2 {
 	@Test
 	public void TestMethod1()
 	{
+		System.out.println("Hello Second statement from Test_Class2");
 //		System.out.println("Hello Second statement from Test_Class2");
 	}
 
