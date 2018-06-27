96a93edc5afa97d40d68b925952d91286b6ce35e
Ashok N T <ANT2@ANT2W10BLR.dsone.3ds.com>
2018-06-27 12:21:15 +0530

    Commit from eclipse without taking latest from GitHub

--- src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
+++ src/test/java/SpecialBatch3_POM_FW/SpecialBatch3_POM_FW/Test_class2.java
@@ -8,7 +8,7 @@ public class Test_class2 {
 	public void TestMethod1()
 	{
 		System.out.println("Hello Second statement from Test_Class2");
-		System.out.println("Hello Test_Class2");
+		System.out.println("Hello Test_Class2 - from eclipse");
 		System.out.println("Done from Eclipse");
 		
 	}
